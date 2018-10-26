d2b7743977ee9f6ef440548e328121f727b88f61
dave <angelnmara@hotmail.com>
2018-10-26 15:56:08 -0500

    first commit

---
+++ app/src/main/java/mx/com/lamarrulla/transporttracker/TrackerService.java
@@ -0,0 +1,77 @@
+package mx.com.lamarrulla.transporttracker;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class TrackerService extends Service {
+    private static final String TAG = TrackerService.class.getSimpleName();
+    String NOTIFICATION_CHANNEL_ID;
+
+    @Override
+    public IBinder onBind(Intent intent) {return null;}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildNotification();
+        loginToFirebase();
+    }
+
+    private void buildNotification() {
+
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
+            String channelName = "My Background Service";
+            NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+            chan.setLightColor(Color.BLUE);
+            chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert manager != null;
+            manager.createNotificationChannel(chan);
+        }
+
+        // Create the persistent notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.notification_text))
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.ic_tracker);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "received stop broadcast");
+            // Stop the service when the notification is tapped
+            unregisterReceiver(stopReceiver);
+            stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+        // Functionality coming next step
+    }
+
+    private void requestLocationUpdates() {
+        // Functionality coming next step
+    }
+}
